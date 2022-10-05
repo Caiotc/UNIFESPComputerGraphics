f9de8bcabc80501f582f483f4f4fc6c2008a7676
Caio Theodoro de Carvalho <caio.theodoroc@gmail.com>
2022-10-05 00:56:39 -0300

    finalizing window creating and first image ploted

--- WindowSystems/firstSteps.c
+++ WindowSystems/firstSteps.c
@@ -1,3 +1,8 @@
+/**
+ * This shit assa program only display a black window and a green line
+ *
+ * **/
+
 // #include <windows.h>
 // #include <GL/gl.h>
 // #include <GL/glu.h>
@@ -11,15 +16,53 @@ gonna include GL/glut.h
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+void init()
+{
+    // this sets a windows background color RGB,ALPHA
+    glClearColor(1.0, 1.0, 1.0, 0.0);
 
-void lineSegment() {}
+    glMatrixMode(GL_PROJECTION);
+    // Set projection parameters.
+    // defines the coordinate reference  frame within
+    // the display window
+    gluOrtho2D(0.0, 200.0, 0.0, 150.0);
+}
+void lineSegment()
+{
+    glClear(GL_COLOR_BUFFER_BIT); // Clear Display window
+
+    glColor3f(0.0, 0.4, 0.2); // set figure color => green
+    glBegin(GL_LINES);
+    glVertex2i(180, 15);
+    glVertex2i(10, 145);
+    glEnd();
+
+    glFlush(); // process opengl quick as possible
+}
 
 int main(int argc, char *argv[])
 {
     // glutInit also process any command line arguments
     glutInit(&argc, argv);
+    // we can set other options for the display window
+    // such as buffering and a choice of color modes
+    //  SINGLE MODE AND RGB ARE THE DEFAULT VALUE
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    /*
+     there are two functions that can be used
+     to set the window position and size
+    here i set them with their default value
+    - same shit if we just ignore both of them
+    */
+    glutInitWindowPosition(50, 100);
+    glutInitWindowSize(400, 600);
+    //////////////////////////////
+
     // the window will be created with a given title  bar
     glutCreateWindow("This is the first going throuygh this shit");
+
+    init();
+
     // well, this assigns the picture definition to the display window
     glutDisplayFunc(lineSegment);
 
